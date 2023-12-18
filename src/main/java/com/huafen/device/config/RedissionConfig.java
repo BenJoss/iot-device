357737647909428d61eb2a79c274512e9d46369b
BenJoss <993500770@qq.com>
2023-12-19 00:04:23 +0800

    准生产环境校验

--- src/main/java/com/huafen/device/config/RedissionConfig.java
+++ src/main/java/com/huafen/device/config/RedissionConfig.java
@@ -5,11 +5,11 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.config.SingleServerConfig;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import com.huafen.device.model.config.RedisProperties;
 
-@Configuration
+//@Configuration
+// TODO
+@Deprecated
 public class RedissionConfig {
 
     /**
